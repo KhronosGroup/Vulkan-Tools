doraskayo/Vulkan-LoaderAndValidationLayers

layers: Move too many images case to CV
Signed-off-by: Chris Forbes <chrisforbes@google.com>

layers/core_validation.cpp
@@ -11692,6 +11692,19 @@ VKAPI_ATTR VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainK
     if (pFence) {
         skip_call |= ValidateFenceForSubmit(dev_data, pFence);
     }
+
+    auto swapchain_data = getSwapchainNode(dev_data, swapchain);
+    auto physical_device_state = getPhysicalDeviceState(dev_data->instance_data, dev_data->gpu);
+    if (physical_device_state->vkGetPhysicalDeviceSurfaceCapabilitiesKHRState != UNCALLED) {
+        int acquired_images = std::count_if(swapchain_data->images.begin(), swapchain_data->images.end(),
+                                            [=](VkImage image) { return getImageState(dev_data, image)->acquired; });
+        if (acquired_images > swapchain_data->images.size() - physical_device_state->surfaceCapabilities.minImageCount) {
+            skip_call |= log_msg(
+                dev_data->report_data, VK_DEBUG_REPORT_ERROR_BIT_EXT, VK_DEBUG_REPORT_OBJECT_TYPE_SWAPCHAIN_KHR_EXT,
+                reinterpret_cast<uint64_t const &>(swapchain), __LINE__, DRAWSTATE_SWAPCHAIN_TOO_MANY_IMAGES, "DS",
+                "vkAcquireNextImageKHR: Application has already acquired the maximum number of images (%d)", acquired_images);
+        }
+    }
     lock.unlock();
 
     if (skip_call)
@@ -11713,7 +11726,6 @@ VKAPI_ATTR VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainK
         }
 
         // Mark the image as acquired.
-        auto swapchain_data = getSwapchainNode(dev_data, swapchain);
         auto image = swapchain_data->images[*pImageIndex];
         auto image_state = getImageState(dev_data, image);
         image_state->acquired = true;

KhronosGroup/Vulkan-Tools

layers/core_validation.cpp
@@ -12003,6 +12003,19 @@ VKAPI_ATTR VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainK
     if (pFence) {
         skip_call |= ValidateFenceForSubmit(dev_data, pFence);
     }
+
+    auto swapchain_data = getSwapchainNode(dev_data, swapchain);
+    auto physical_device_state = getPhysicalDeviceState(dev_data->instance_data, dev_data->physical_device);
+    if (physical_device_state->vkGetPhysicalDeviceSurfaceCapabilitiesKHRState != UNCALLED) {
+        int acquired_images = std::count_if(swapchain_data->images.begin(), swapchain_data->images.end(),
+                                            [=](VkImage image) { return getImageState(dev_data, image)->acquired; });
+        if (acquired_images > swapchain_data->images.size() - physical_device_state->surfaceCapabilities.minImageCount) {
+            skip_call |= log_msg(
+                dev_data->report_data, VK_DEBUG_REPORT_ERROR_BIT_EXT, VK_DEBUG_REPORT_OBJECT_TYPE_SWAPCHAIN_KHR_EXT,
+                reinterpret_cast<uint64_t const &>(swapchain), __LINE__, DRAWSTATE_SWAPCHAIN_TOO_MANY_IMAGES, "DS",
+                "vkAcquireNextImageKHR: Application has already acquired the maximum number of images (%d)", acquired_images);
+        }
+    }
     lock.unlock();
 
     if (skip_call)
@@ -12024,7 +12037,6 @@ VKAPI_ATTR VkResult VKAPI_CALL AcquireNextImageKHR(VkDevice device, VkSwapchainK
         }
 
         // Mark the image as acquired.
-        auto swapchain_data = getSwapchainNode(dev_data, swapchain);
         auto image = swapchain_data->images[*pImageIndex];
         auto image_state = getImageState(dev_data, image);
         image_state->acquired = true;