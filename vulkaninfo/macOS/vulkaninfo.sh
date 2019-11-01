d5b8bdcdf7f47f9bd7846cd297c506d62c7fe3a4
Jeremy Kniager <jeremyk@lunarg.com>
2019-11-01 09:48:39 -0600

    vulkaninfo: Fix gitlab issue 181
    Fixes vulkaninfo.app bundle crashing on startup.
    
    Change-Id: I515611c28da35a0f55edcb8492d26ad78046c0c2

--- vulkaninfo/macOS/vulkaninfo.sh
+++ vulkaninfo/macOS/vulkaninfo.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 BASEDIR=`dirname $0`
-open /Applications/Utilities/Terminal.app $BASEDIR/vulkaninfo
+
+if [ -d /System/Applications/Utilities/Terminal.app ]
+then
+    open /System/Applications/Utilities/Terminal.app $BASEDIR/vulkaninfo
+else
+    open /Applications/Utilities/Terminal.app $BASEDIR/vulkaninfo
+fi
