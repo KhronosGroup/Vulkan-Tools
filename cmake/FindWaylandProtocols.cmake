b7137fa758c28cac1ec3fe6172b3bd0ada527728
Juan Ramos <juan@lunarg.com>
2022-10-26 10:38:01 -0600

    cmake: Suppress Wayland warnings
    The warnings are purely semantic.

--- cmake/FindWaylandProtocols.cmake
+++ cmake/FindWaylandProtocols.cmake
@@ -9,6 +9,10 @@ if(NOT WIN32)
     pkg_check_modules(WAYLAND_PROTOCOLS_PATH REQUIRED wayland-protocols)
     execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable=pkgdatadir wayland-protocols
        OUTPUT_VARIABLE WAYLAND_PROTOCOLS_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    set(FPHSA_NAME_MISMATCHED ON)
     find_package_handle_standard_args(WAYLAND DEFAULT_MSG WAYLAND_PROTOCOLS_PATH)
+    unset(FPHSA_NAME_MISMATCHED)
+
     mark_as_advanced(WAYLAND_PROTOCOLS_PATH)
 endif()
