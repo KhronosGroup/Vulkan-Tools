037d85128456591f6e33319c28244ad5a49cf1ad
Juan Ramos <juan@lunarg.com>
2022-10-26 10:38:01 -0600

    cmake: Suppress Wayland warnings
    The warnings are purely semantic.

--- cmake/FindWaylandScanner.cmake
+++ cmake/FindWaylandScanner.cmake
@@ -9,6 +9,10 @@ if(NOT WIN32)
     find_package(PkgConfig)
     pkg_check_modules(PKG_WAYLAND_SCANNER QUIET wayland-scanner)
     find_program(WAYLAND_SCANNER_EXECUTABLE wayland-scanner ${PKG_WAYLAND_SCANNER_PREFIX}/bin/wayland-scanner)
+
+    set(FPHSA_NAME_MISMATCHED ON)
     find_package_handle_standard_args(WAYLAND DEFAULT_MSG WAYLAND_SCANNER_EXECUTABLE)
+    unset(FPHSA_NAME_MISMATCHED)
+
     mark_as_advanced(WAYLAND_SCANNER_EXECUTABLE)
 endif()
