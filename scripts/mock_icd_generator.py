daad82cc24345d5cf02bffc310f7ae87a869c6cf
Artem Kharytoniuk <artem@lunarg.com>
2024-04-01 16:28:41 +0200

    icd: Add external memory properties

--- scripts/mock_icd_generator.py
+++ scripts/mock_icd_generator.py
@@ -531,9 +531,8 @@ CUSTOM_C_INTERCEPTS = {
 'vkGetPhysicalDeviceImageFormatProperties2KHR': '''
     auto *external_image_prop = lvl_find_mod_in_chain<VkExternalImageFormatProperties>(pImageFormatProperties->pNext);
     auto *external_image_format = lvl_find_in_chain<VkPhysicalDeviceExternalImageFormatInfo>(pImageFormatInfo->pNext);
-    if (external_image_prop && external_image_format && external_image_format->handleType == VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID) {
+    if (external_image_prop && external_image_format) {
         external_image_prop->externalMemoryProperties.externalMemoryFeatures = VK_EXTERNAL_MEMORY_FEATURE_IMPORTABLE_BIT | VK_EXTERNAL_MEMORY_FEATURE_EXPORTABLE_BIT;
-        external_image_prop->externalMemoryProperties.compatibleHandleTypes = external_image_format->handleType;
         external_image_prop->externalMemoryProperties.compatibleHandleTypes = external_image_format->handleType;
     }
 
@@ -1667,6 +1666,10 @@ CUSTOM_C_INTERCEPTS = {
     }
     return VK_SUCCESS;
 ''',
+'vkGetMemoryWin32HandlePropertiesKHR': '''
+    pMemoryWin32HandleProperties->memoryTypeBits = 0xFFFF;
+    return VK_SUCCESS;
+'''
 }
 
 # MockICDGeneratorOptions - subclass of GeneratorOptions.
